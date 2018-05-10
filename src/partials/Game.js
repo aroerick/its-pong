e63894b3ea6797cacc25ec402a190ad1d699d0da
aroerick <a.roerick@gmail.com>
2018-05-10 10:57:02 -0700

    Rough base SVG

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,11 +1,13 @@
+import { SVG_NS } from "../settings"
+
 export default class Game {
 
 	constructor(element, width, height) {
 		this.element = element;
 		this.width = width;
 		this.height = height;
-	
-		// Other code goes here...
+
+		this.gameElement = document.getElementById(this.element);
 	}
 
 	render() {
